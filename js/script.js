5d898fa8397d9d628ad571931375c72ee3ff6ca4
Vadym <vadosevich@gmail.com>
2016-11-07 19:34:29 +0200

    Form

--- js/script.js
+++ js/script.js
@@ -1,22 +1,54 @@
 $(document).ready(function(){
 
-var FREQ = 10000;	
+var repeat = true;	
+var FREQ = 10000;
 
 function startAJAXcalls(){
+
+	if(repeat) {
 
 	setTimeout(function() {
 
 		getXMLRacers();
 		startAJAXcalls();
+
 		},
 		FREQ );
+	}
+
 };
+
+$("#btnStop").click(function(){
+
+	repeat = false;	
+
+	$("#freq").html("«Updates paused»");
+
+    });
+
+$("#btnStart").click(function(){
+
+	repeat = true; 	
+
+	$("#freq").html("«Start updates»");	
+
+	startAJAXcalls();
+
+	showFrequency();
+
+});
 
 getXMLRacers();
 
 startAJAXcalls()
 
+function showFrequency(){
+	$("#freq").html("Page refreshes every " + FREQ/1000 + " second(s).");
+}
 
+function getTimeAjax(){
+	$("#updatedTime").load("time.php");
+}
 
 function getXMLRacers(){
 
@@ -44,27 +76,11 @@ function getXMLRacers(){
 					$('#finishers_all').append(info);
 			});
 
-			getTime();
+			getTimeAjax();
 		}	
 
 	});
 
 };
 
-
-	function getTime(){
-		var a_p = "";
-		var d = new Date();
-		var curr_hour = d.getHours();
-			(curr_hour < 12) ? a_p = "AM" : a_p = "PM";
-			(curr_hour == 0) ? curr_hour = 12 : curr_hour = curr_hour;
-			(curr_hour > 12) ? curr_hour = curr_hour - 12 : curr_hour = curr_hour;
-		var curr_min = d.getMinutes().toString();
-		var curr_sec = d.getSeconds().toString();
-	if (curr_min.length == 1) { curr_min = "0" + curr_min; }
-	if (curr_sec.length == 1) { curr_sec = "0" + curr_sec; }
-
-	$('#updatedTime').html(curr_hour + ":" + curr_min + ":" + curr_sec + " " + a_p );
-
-	}
 });
